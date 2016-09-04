321fef612170758fc0c02040c0d435a10f358a6d
Michael Deal <mudcube@gmail.com>
2016-09-03 19:25:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function (items, handler, processes) {
 	var _keys = []
 	var _items = []
 
-	var _promise = Galactic.emitter(new Promise((resolve, reject) => {
+	var _promise = EventEmitter(new Promise((resolve, reject) => {
 		_resolve = resolve
 		_reject = reject
 		queue.cancel = cancel
@@ -93,7 +93,7 @@ module.exports = function (items, handler, processes) {
 		_resolve(_results)
 	}
 
-	function cancel(message = 'No reason provided') {
+	function cancel(message) {
 		if (_done) {
 			return
 		}
@@ -105,4 +105,132 @@ module.exports = function (items, handler, processes) {
 			results: _results
 		})
 	}
+}
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function EventEmitter(proto) {
+	proto || (proto = {});
+	if (proto.on || proto.off || proto.emit) {
+		return proto; // already has handlers
+	}
+
+	var always = {};
+	var handlers = {};
+	var uniqueId = 1;
+
+	function createId(type, handler) {
+		if (typeof handler !== 'function') {
+			console.error(type + ' listener is undefined');
+			return;
+		}
+
+		var handlerId = handler.uniqueId || (handler.uniqueId = uniqueId++);
+		return type + '.' + handlerId;
+	}
+
+	function listener(type) {
+		if (handlers[type]) {
+			return handlers[type];
+		} else {
+			return handlers[type] = Stack(type);
+		}
+	}
+
+	function Stack(type) {
+		var stack = {};
+
+		function emitter() {
+			var overrides = false;
+			for (var key in stack) {
+				if (stack[key].apply(proto, arguments)) {
+					overrides = true;
+				}
+			}
+			return overrides;
+		}
+
+		emitter.add = function (handler, that) {
+			var fid = createId(type, handler);
+			if (stack[fid] === undefined && handler) {
+				stack[fid] = handler;
+			}
+			return {
+				add: function add() {
+					stack[fid] = handler;
+				},
+				remove: function remove() {
+					delete stack[fid];
+				}
+			};
+		};
+
+		emitter.remove = function (handler) {
+			var fid = createId(type, handler);
+			if (stack[fid] !== undefined && handler) {
+				delete stack[fid];
+			}
+		};
+
+		return emitter;
+	}
+
+	proto.on = function (type, handler) {
+		if (always[type]) {
+			handler.apply(undefined, _toConsumableArray(always[type]));
+		}
+
+		var emitter = listener(type).add(handler);
+		var out = toEmitter(this);
+		out.add = emitter.add; //- depreciate
+		out.remove = emitter.remove; //- depreciate
+		out.on = this.on;
+		out.off = this.off;
+		out.emit = this.emit;
+		return out;
+	};
+
+	proto.on.handlers = handlers;
+
+	proto.off = function (type, handler) {
+		if (handler) {
+			listener(type).remove(handler);
+		} else {
+			delete handlers[type];
+		}
+	};
+
+	proto.emit = function (type) {
+		var stack = handlers[type];
+		if (stack) {
+			var args = Array.prototype.slice.call(arguments).slice(1);
+			return stack.apply(proto, args);
+		}
+	};
+
+	proto.emit.andNotifyNewHandlers = function (type) {
+		var _proto;
+
+		always[type] = arguments;
+		(_proto = proto).emit.apply(_proto, arguments);
+	};
+
+	proto.emit.promiseRequest = function (type) {
+		//- depreciate
+		return new Promise(function (resolve, reject) {
+			proto.emit(type, resolve, reject);
+		});
+	};
+	return proto;
+}
+
+function toEmitter(object) {
+	if (!object.then) {
+		return {};
+	}
+
+	if (object.on && object.off && object.emit) {
+		return object;
+	}
+	return EventEmitter(object);
 }
